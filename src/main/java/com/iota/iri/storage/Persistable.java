55df620ca48bedb48ac393125a7692e0e5cfc2ee
Paul D Handy <paul.d.handy@gmail.com>
2017-05-07 00:27:07 -0600

    make persistable extend serializable

--- src/main/java/com/iota/iri/storage/Persistable.java
+++ src/main/java/com/iota/iri/storage/Persistable.java
@@ -1,9 +1,11 @@
 package com.iota.iri.storage;
 
+import java.io.Serializable;
+
 /**
  * Created by paul on 5/6/17.
  */
-public interface Persistable {
+public interface Persistable extends Serializable {
     byte[] bytes();
     void read(byte[] bytes);
 }
